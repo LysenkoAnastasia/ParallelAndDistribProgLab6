bb6c203157e52dfe60f79953d729152c37f2a63b
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 20:54:14 +0300

    Scripted auto-commit on change (2019-12-07 20:54:14) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
@@ -1,13 +1,29 @@
 package ru.bmstu.zookeeper.lab6;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
 import akka.stream.javadsl.Flow;
+import org.apache.zookeeper.ZooKeeper;
+import org.asynchttpclient.AsyncHttpClient;
 
 public class Anonymization {
+    AsyncHttpClient asyncHttpClient;
+    Materializer materializer;
+    ActorRef storage;
+    ZooKeeper zoo;
 
-    public Anonymization(Ass)
+    public Anonymization(AsyncHttpClient asyncHttpClient, ActorSystem system, ActorMaterializer materializer, ZooKeeper zoo) {
+        this.materializer = materializer;
+        this.asyncHttpClient = asyncHttpClient;
+        this.storage = system.actorOf(Props.create(StorageActor.class));
+        this.zoo = zoo;
+    }
 
     public Flow<HttpRequest, HttpResponse, NotUsed> createRoute() {
     }
