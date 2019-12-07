e4e983a3feb83d11b4e0164dbd10e39767485162
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 20:46:20 +0300

    Scripted auto-commit on change (2019-12-07 20:46:20) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
@@ -1,4 +1,12 @@
 package ru.bmstu.zookeeper.lab6;
 
+import akka.NotUsed;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.javadsl.Flow;
+
 public class Anonymization {
+
+    public Flow<HttpRequest, HttpResponse, NotUsed> createRoute() {
+    }
 }
