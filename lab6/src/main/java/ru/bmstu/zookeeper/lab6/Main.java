bf4ae4cac3484e1a1e8b0e9ce7583d89d2dc679e
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 20:50:39 +0300

    Scripted auto-commit on change (2019-12-07 20:50:39) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
@@ -12,6 +12,7 @@ import akka.stream.javadsl.Flow;
 import org.asynchttpclient.AsyncHttpClient;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
@@ -19,7 +20,7 @@ import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
         ActorSystem system = ActorSystem.create("routes");
@@ -40,16 +41,5 @@ public class Main {
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
-    }
-
-       /* zoo.create("/servers/s", .getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
-                CreateMode.EPHEMERAL_SEQUENTIAL
-);
-        List<String> servers = zoo.getChildren("/servers", this);
-        for (String s : servers) {
-            byte[] data = zoo.getData("/servers/" + s, false, null);
-            System.out.println("server " + s + " data=" + new String(data));
-        }*/
     }
 }
