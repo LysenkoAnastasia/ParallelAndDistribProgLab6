1ba948712f0fb4edce4300f45886058c9132c7f9
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 20:36:27 +0300

    Scripted auto-commit on change (2019-12-07 20:36:27) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
@@ -1,5 +1,20 @@
 package ru.bmstu.zookeeper.lab6;
 
+import org.apache.zookeeper.ZooKeeper;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
+        zoo.create("/servers/s",
+                “data”.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+);
+        List<String> servers = zoo.getChildren("/servers", this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
+    }
 }
