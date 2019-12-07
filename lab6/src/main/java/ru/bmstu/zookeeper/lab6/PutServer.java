98503dade10ce594a379e5289565d7756280f46e
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 20:32:23 +0300

    Scripted auto-commit on change (2019-12-07 20:32:23) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/PutServer.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/PutServer.java
@@ -5,4 +5,11 @@ import java.util.ArrayList;
 public class PutServer {
     private ArrayList<String> server;
 
+    private PutServer(ArrayList<String> server) {
+        this.server = server;
+    }
+
+    public ArrayList<String> getServer() {
+        return server;
+    }
 }
