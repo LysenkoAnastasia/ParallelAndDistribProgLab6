982115d1995cd20e6aa971d3fa0ee7c523d71feb
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 20:39:41 +0300

    Scripted auto-commit on change (2019-12-07 20:39:41) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
@@ -1,5 +1,8 @@
 package ru.bmstu.zookeeper.lab6;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
@@ -9,8 +12,14 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("start!");
         ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
-        
+        ActorSystem system = ActorSystem.create("routes");
+        AsyncHttpClient asyncHttpClient = asyncHttpClient();
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+
        /* zoo.create("/servers/s", .getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE ,
                 CreateMode.EPHEMERAL_SEQUENTIAL
