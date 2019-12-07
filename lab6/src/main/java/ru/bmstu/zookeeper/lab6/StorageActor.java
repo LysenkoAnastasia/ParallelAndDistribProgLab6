bb1a10fc7d8523722605d29f012ac69cad9efad1
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 19:39:20 +0300

    Scripted auto-commit on change (2019-12-07 19:39:20) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/StorageActor.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/StorageActor.java
@@ -1,15 +1,18 @@
 package ru.bmstu.zookeeper.lab6;
 
 import akka.actor.AbstractActor;
+import scala.Array;
 import scala.collection.immutable.List;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class StorageActor extends AbstractActor {
-    private List<String> storage ; 
+    private ArrayList<String> storage ;
 
 
     public StorageActor() {
+        this.storage = new ArrayList<>();
 
     }
 
