059eddaae1460927317984feb63d9411f89d51ad
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-07 20:32:50 +0300

    Scripted auto-commit on change (2019-12-07 20:32:50) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/StorageActor.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/StorageActor.java
@@ -33,8 +33,8 @@ public class StorageActor extends AbstractActor {
     }
 
     private void putServer(PutServer putServer) {
-        this.storage.clear();
-        this.storage.addAll(putServer)
+        //this.storage.clear();
+        this.storage.addAll(putServer.getServer());
 
     }
 }
