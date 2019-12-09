5c47311b2d54b7a4394d7dd575c4efbf0dea8411
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-09 09:57:46 +0300

    Scripted auto-commit on change (2019-12-09 09:57:46) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Server.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Server.java
@@ -3,9 +3,11 @@ package ru.bmstu.zookeeper.lab6;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class Server {
     private ZooKeeper zoo;
+    private Logger log;
 
     public Server(String connectString) throws IOException {
         this.zoo = new ZooKeeper(connectString, 3000, this);
