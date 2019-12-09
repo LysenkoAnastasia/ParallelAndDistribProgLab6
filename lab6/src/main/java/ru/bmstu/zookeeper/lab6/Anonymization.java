98516187c6a629420055a4e91039415baed0f535
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-09 09:26:21 +0300

    Scripted auto-commit on change (2019-12-09 09:26:21) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
@@ -65,8 +65,7 @@ public class Anonymization {
 
     private CompletionStage<Response> redirectiv(String url, int count) {
         FutureConverters.toJava(Patterns.ask(storage, new GetRandom(), 5000))
-                .thenApply(s -> (String)s)
-                .thenApply()
+                .thenApply(s -> ((ReturnServerMsg)s).getServer())
                 .thenCompose(server -> fetch()
 
                 )
