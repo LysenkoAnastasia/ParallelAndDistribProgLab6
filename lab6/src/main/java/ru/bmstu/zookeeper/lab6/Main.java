c5182bc39fb198c3f1fd7ffc441354726a7d2f7e
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-10 14:59:15 +0300

    Scripted auto-commit on change (2019-12-10 14:59:15) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Main.java
@@ -40,7 +40,8 @@ public class Main {
         final Http http = Http.get(system);
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
-        Server server = new Server(zoo, )
+        Server server = new Server(zoo, storage);
+        server.createServer(host, port);
         Anonymization app = new Anonymization(asyncHttpClient, storage, materializer, zoo, http);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
