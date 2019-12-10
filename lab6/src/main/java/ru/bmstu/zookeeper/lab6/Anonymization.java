f78bb17dcc29fa14f5442a38fd8ccc96688e6c21
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-10 13:24:15 +0300

    Scripted auto-commit on change (2019-12-10 13:24:15) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Anonymization.java
@@ -80,6 +80,6 @@ public class Anonymization {
     }
 
     private CompletionStage<HttpResponse> fetch(String  url) {
-        return http.singleRequest(HttpRequest.create(url));
+        return http.singleRequest(HttpRequest.GET(url));
     }
 }
