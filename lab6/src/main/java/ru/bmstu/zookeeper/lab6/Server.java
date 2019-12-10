b75eff2514653da679865cca1573e1dee1031d92
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-10 11:47:16 +0300

    Scripted auto-commit on change (2019-12-10 11:47:16) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/Server.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/Server.java
@@ -21,7 +21,7 @@ public class Server {
 
     }
 
-    public void createServer(String host, String port, String name) throws Exception {
+    public void createServer(String host, String port) throws Exception {
         zoo.create(
                 "/servers", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT
         );
