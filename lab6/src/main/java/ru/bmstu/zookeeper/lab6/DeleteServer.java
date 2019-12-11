b637cc5e42ff343bb262137065ea9e45aad996a0
LysenkoAnastasia <lysenko2712@mail.ru>
2019-12-11 21:39:28 +0300

    Scripted auto-commit on change (2019-12-11 21:39:28) by gitwatch.sh

--- lab6/src/main/java/ru/bmstu/zookeeper/lab6/DeleteServer.java
+++ lab6/src/main/java/ru/bmstu/zookeeper/lab6/DeleteServer.java
@@ -1,5 +1,13 @@
 package ru.bmstu.zookeeper.lab6;
 
 public class DeleteServer {
+    private String server;
 
+    public DeleteServer(String server) {
+        this.server = server;
+    }
+
+    public String getServer() {
+        return server;
+    }
 }
